3c0df1bcfce87cbcebb5eb5eb7775616e64fb3aa
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-07-14 17:29:11 +0700

    Finish ref_out

---
+++ Lesson2/LearnFunction/Demo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace LearnFunction
+{
+    public class Demo
+    {
+        public double CalRec(double width, double length)
+        {
+            double area= width* length;
+            return area;
+        }
+
+
+        public void RunCalRec()
+        {
+            Console.WriteLine("Input the width");
+            double.TryParse(Console.ReadLine(), out double width);
+
+            Console.WriteLine("Input the leght");
+           
+
+            try
+            {
+                double.TryParse(Console.ReadLine(), out double leght);
+
+                var result = CalRec(width, leght);
+                Console.WriteLine($"diện tích là HCN có chiều rộng {width} và chiều dài {leght} là : {result}");
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Leght is not correct");
+            }
+        }
+
+        //viết hàm tính diện tích hình tròn
+        // viết hàm run tính dt hình tròn
+
+
+        public double CalCircle(double rad, out double perimeter)
+        {
+            double area= Math.PI*rad*rad;   //pi*(R bình)
+            perimeter = rad * 2 * Math.PI;
+            return area;
+
+        }
+
+        public void RunCalCircle()
+        {
+            Console.WriteLine("Input the radius of the circle");
+            // nếu đổi được thì rad= số, tryParse= true
+            // nếu ko đổi ra số dc tryParse=fasle ,  rad=0
+           double.TryParse(Console.ReadLine(), out double rad); // kiểu ném ra giống try catch
+
+            // nhận out ném ra
+            /* var result = CalCircle(rad, out double perimeter);
+             Console.WriteLine($"diện tích của Tròn có bán kính  {rad} là : {result} và chu vi : {perimeter}");*/
+
+            //không nhận out ném ra
+            var result = CalCircle(rad, out double _);
+            Console.WriteLine($"diện tích của Tròn có bán kính  {rad} là : {result} ");
+        }
+
+        // thay đổi giá trị 
+        public void ChangeValue(int number)
+        {
+            number += 5;
+
+        }
+
+        public void ChangeValue(ref int number)
+        {
+            number += 5;
+
+        }
+
+    }
+}
---
+++ Lesson2/LearnFunction/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LearnFunction;
+using System.Runtime.CompilerServices;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.InputEncoding = Encoding.UTF8;
+
+//Console.WriteLine("Chương Trình Tính Diện Tích HCN!");
+Demo demo= new Demo();
+double result = demo.CalRec(3, 5);
+
+//Demo demo1= new Demo();
+//demo1.RunCalRec();
+
+//Demo demo2= new Demo();
+//demo2.RunCalCircle();
+int number = 10;
+demo.ChangeValue(number);   //ko làm thay giá trị tham sô ban đầu - tham trị
+demo.ChangeValue(ref number);   // làm thay giá trị tham sô ban đầu - tham chiếu
+
+Console.ReadLine();
+
+
