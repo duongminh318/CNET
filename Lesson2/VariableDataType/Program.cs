fc3462c383a8f3ecd04b938c2168ef2b4a60f1ba
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-07-14 14:08:24 +0700

    create_brand_lesosn2

---
+++ Lesson2/VariableDataType/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Input the Name");
+
+string name=Console.ReadLine();
+Console.WriteLine("tên của thím là: "+name);
+
+Console.WriteLine("Input the age");
+
+int age =int.Parse( Console.ReadLine());
+Console.WriteLine("tuổi của thím là: " + age);
+
+Console.WriteLine("Input the address");
+
+string address = Console.ReadLine();
+Console.WriteLine("Địa chỉ của thím là: " + address);
+
+Console.ReadKey();
