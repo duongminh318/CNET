a1efd1eab75da9ccdc6888fbd48fba40cdf8dc2a
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-07-21 16:17:54 +0700

    Lesson4 Swith case_ Enum_contant

---
+++ Lesson4/LearnSwitchCase/Month.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnSwitchCase
+{
+    public enum Month
+    {
+        January = 1,
+        February = 2,
+        March = 3,
+        April = 4,
+        May = 5,
+        June = 6,
+        July = 7,
+        August = 8,
+        September = 9,
+        Octorber = 10,
+        November = 11,
+        December = 12
+
+    }
+}
---
+++ Lesson4/LearnSwitchCase/Program.cs
@@ -0,0 +1,10 @@
+﻿using LearnSwitchCase;
+
+var dm1 = new Demo();
+//dm1.ShowMessage(200);
+//dm1.RunGetDaysInMonth();
+dm1.RunGetDaysInMonth2();
+
+var values =(int)Month.January;
+var values2 =Month.January;
+Console.ReadKey();
