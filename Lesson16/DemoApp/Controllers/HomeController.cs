ca9214fe64cf8388e5ae7bea68374aca55dcc101
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-09-22 17:12:53 +0700

    16. list blog controler

--- Lesson16/DemoApp/Controllers/HomeController.cs
+++ Lesson16/DemoApp/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-using DemoApp.Models;
+﻿using DemoApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,9 +7,39 @@ namespace DemoApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private readonly List<BlogViewModel> _blogList;
         public HomeController(ILogger<HomeController> logger)
         {
+            _blogList = new List<BlogViewModel>()
+            {
+                new BlogViewModel
+                {
+                 Id = Guid.Parse("c02300aa-11d6-44b8-9f8a-92b97a333721"),
+                 Name = "Hướng dẫn lập trình",
+                 Image = "https://media.istockphoto.com/id/1646501089/photo/closeup-group-of-asian-people-software-developers-using-computer-to-write-code-sitting-at.webp?a=1&b=1&s=612x612&w=0&k=20&c=7OC1ykUyXbgHImSLIevBvmwvbn6K7ys0JZAyvXqf2zQ="
+                },
+
+                 new BlogViewModel
+                {
+                 Id = Guid.Parse("ccb61820-b975-45b7-8b3d-bc2d78ce9f2e"),
+                 Name = "Hướng dẫn lập trình",
+                 Image = "https://media.istockphoto.com/id/1646501089/photo/closeup-group-of-asian-people-software-developers-using-computer-to-write-code-sitting-at.webp?a=1&b=1&s=612x612&w=0&k=20&c=7OC1ykUyXbgHImSLIevBvmwvbn6K7ys0JZAyvXqf2zQ="
+                },
+
+                  new BlogViewModel
+                {
+                 Id = Guid.Parse("32b2decd-6ba9-4544-880a-b6e8d2c4e314"),
+                 Name = "Hướng dẫn lập trình",
+                 Image = "https://media.istockphoto.com/id/1646501089/photo/closeup-group-of-asian-people-software-developers-using-computer-to-write-code-sitting-at.webp?a=1&b=1&s=612x612&w=0&k=20&c=7OC1ykUyXbgHImSLIevBvmwvbn6K7ys0JZAyvXqf2zQ="
+                },
+                   new BlogViewModel
+                {
+                 Id = Guid.Parse("c6442127-afde-42f1-bcc6-17c9243c4edb"),
+                 Name = "Hướng dẫn lập trình",
+                 Image = "https://media.istockphoto.com/id/1646501089/photo/closeup-group-of-asian-people-software-developers-using-computer-to-write-code-sitting-at.webp?a=1&b=1&s=612x612&w=0&k=20&c=7OC1ykUyXbgHImSLIevBvmwvbn6K7ys0JZAyvXqf2zQ="
+                },
+
+            };
             _logger = logger;
         }
 
@@ -18,27 +48,21 @@ namespace DemoApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-       /* public IActionResult Blog()
-        {
-            return View();
-        }*/
-
         public IActionResult Blog()
         {
-            return View();
             var blog = new BlogViewModel
             {
                 Id = Guid.NewGuid(),
-                Name = "H�?ng d?n l?p tr?nh",
+                Name = "Hướng dẫn lập trình",
                 Image = "https://media.istockphoto.com/id/1646501089/photo/closeup-group-of-asian-people-software-developers-using-computer-to-write-code-sitting-at.webp?a=1&b=1&s=612x612&w=0&k=20&c=7OC1ykUyXbgHImSLIevBvmwvbn6K7ys0JZAyvXqf2zQ="
             };
             return View(blog);
         }
 
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
