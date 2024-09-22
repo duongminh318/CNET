91ac38c0bc7f4bb71393240d6876e3e7b3bc20d6
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-09-22 13:53:42 +0700

    15. VariantDetailViewModel

---
+++ lesson15/LearnEfCore/Models/VariantDetailViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnEfCore.Models
+{
+    public class VariantDetailViewModel
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public EntityStatus Status { get; set; }
+        public string StatusLabel => Status.ToString();
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public decimal? DiscountPrice { get; set; }
+        public int Quantity { get; set; }
+        public string ProductName { get; set; }
+    }
+
+}
