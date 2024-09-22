a3c643aa676741c2839492e6fe18797eb4562497
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-09-22 16:19:55 +0700

    16. Add Blog View (demo thêm thử controller --> action blog và view blog)

--- Lesson16/DemoApp/Controllers/HomeController.cs
+++ Lesson16/DemoApp/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace DemoApp.Controllers
         {
             return View();
         }
+        public IActionResult Blog()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
