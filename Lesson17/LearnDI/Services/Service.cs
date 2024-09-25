4b6a3bc417f6786134101c1ad8d08ddda43814e8
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-09-25 21:26:46 +0700

    17. fix Iservice folder

---
+++ Lesson17/LearnDI/Services/Service.cs
@@ -0,0 +1,20 @@
+﻿namespace LearnDI.Services
+{
+    public interface IServiceA
+    {
+        string GetId();
+    }
+
+    public class ServiceA : IServiceA
+    {
+        private readonly Guid _id;
+        public ServiceA()
+        {
+            _id = Guid.NewGuid();
+        }
+        public string GetId()
+        {
+            return _id.ToString();
+        }
+    }
+}
