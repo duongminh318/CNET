4f400ff08f69afb39197332e3f958af43abc6b81
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-09-25 20:06:44 +0700

    16. form

---
+++ Lesson16/DemoApp/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lesson16/DemoApp/Models/CreateStudentViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoApp.Models
+{
+    public class CreateStudentViewModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
