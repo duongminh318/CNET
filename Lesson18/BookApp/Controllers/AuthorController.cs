31d3627db24fc26cecf732166406d6ba0e9745f4
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-07 01:13:02 +0700

    18. CRUD Author (thêm, xoá, sửa author, code controller và view)

---
+++ Lesson18/BookApp/Controllers/AuthorController.cs
@@ -0,0 +1,92 @@
+﻿using BookApp.Services.Authors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApp.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly IAuthorService _authorService;
+        private readonly ILogger<HomeController> _logger;
+        public AuthorController(ILogger<HomeController> logger, IAuthorService authorService)
+        {
+            _authorService = authorService;
+            _logger = logger;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var authors = await _authorService.GetAll();
+            return View(authors);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateAuthorViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            try
+            {
+                await _authorService.Create(model);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        public async Task<IActionResult> Update(Guid id)
+        {
+            var author = await _authorService.GetById(id);
+            var model = new UpdateAuthorViewModel()
+            {
+                Id = author.Id,
+                Name = author.Name,
+
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateAuthorViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            try
+            {
+                await _authorService.Update(model);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                await _authorService.Delete(id);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Lesson18/BookApp/Controllers/BookController.cs
@@ -0,0 +1,121 @@
+﻿using BookApp.Controllers;
+using BookApp.Services.Authors;
+using BookApp.Services.Books;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Ex1RepositoryUOW.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookService _bookService;
+        private readonly IAuthorService _authorService;
+        private readonly ILogger<HomeController> _logger;
+        public BookController(ILogger<HomeController> logger, IBookService bookService, IAuthorService authorService)
+        {
+            _bookService = bookService;
+            _authorService = authorService;
+            _logger = logger;
+        }
+      
+
+        public async Task<IActionResult> Index()
+        {
+            var books = await _bookService.GetAll();
+            return View(books);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var authors = await _authorService.GetAll();
+            return View(authors);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateBookViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                SetError(ModelState);
+            }
+            try
+            {
+                await _bookService.Create(model);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        public async Task<IActionResult> Update(Guid id)
+        {
+            var book = await _bookService.GetById(id);
+            var authors = await _authorService.GetAll();
+            ViewBag.Authors = authors;
+            var model = new UpdateBookViewModel()
+            {
+                Id = book.Id, 
+                Name = book.Name, 
+                AuthorId = book.AuthorId
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UpdateBookViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                SetError(ModelState);
+                return RedirectToAction("Error");
+            }
+            try
+            {
+                await _bookService.Update(model);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                SetError(ModelState);
+                return RedirectToAction("Error");
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                await _bookService.Delete(id);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        private void SetError(ModelStateDictionary modelState)
+        {
+            var errors = new List<string>();
+            foreach (var state in modelState.Values)
+            {
+                foreach (var error in state.Errors)
+                {
+                    errors.Add(error.ErrorMessage);
+                }
+            }
+            TempData["Error"] = string.Join(";", errors);
+           
+        }
+    }
+}
