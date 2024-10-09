049e200001cfe2e4764e56b7874941c938ca5aa4
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-09 20:21:37 +0700

    19. add identity

--- Lesson18/BookApp/Entities/ApplicationDbContext.cs
+++ Lesson18/BookApp/Entities/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Ex1GenericRepositoryUOW.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ex1RepositoryUOW.Entities
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext
     {
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
