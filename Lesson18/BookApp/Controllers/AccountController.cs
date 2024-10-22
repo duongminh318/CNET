62c2dc97d34073e2c35a904e1afed02b7b5a9822
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-22 18:28:44 +0700

    19. Depedency injection (tiêm phụ thuộc)

--- Lesson18/BookApp/Controllers/AccountController.cs
+++ Lesson18/BookApp/Controllers/AccountController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
+
         public IActionResult Login()
         {
             return View();
