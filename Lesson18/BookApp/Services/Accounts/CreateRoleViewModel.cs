3d1c798be7a505aa554ec6b35f9eb1f2f1cb4be6
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-22 18:21:59 +0700

    19. Add CreateRoleViewModel, CreateUserViewModel

---
+++ Lesson18/BookApp/Services/Accounts/CreateRoleViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing.Printing;
+
+namespace BookApp.Services.Accounts
+{
+    public class CreateRoleViewModel
+    {
+        public string RoleName { get; set; }
+    }
+}
---
+++ Lesson18/BookApp/Services/Accounts/CreateUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BookApp.Services.Accounts
+{
+    public class CreateUserViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string RoleId { get; set; }
+
+    }
+}
