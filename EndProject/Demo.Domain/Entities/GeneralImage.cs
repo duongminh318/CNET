977dde902197f579ac856a22e567d30ae75b731b
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-27 17:35:17 +0700

    project. application db context

---
+++ EndProject/Demo.Domain/Entities/GeneralImage.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Demo.Domain.Enums;
+
+namespace Demo.Domain.Entities
+{
+
+    [Table("GeneralImages")]
+    public class GeneralImage : DomainEntity<Guid>, IAuditTable
+    {
+        [Column(TypeName = "nvarchar(1000)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(1000)")]
+        public string Url { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public EntityStatus Status { get; set; }
+    }
+}
---
+++ EndProject/Demo.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Demo.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Persistence;
+
+public class ApplicationDbContext : IdentityDbContext<AppUser, AppRole, Guid>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+    }
+
+    public DbSet<GeneralImage> GeneralImages { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Variant> Variant { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderDetail> OrderDetails { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+    public DbSet<AppUser> AppUsers { get; set; }
+    public DbSet<AppRole> AppRoles { get; set; }
+    public DbSet<Domain.Entities.Permission> Permissions { get; set; }
+    public DbSet<RolePermission> RolesPermissions { get; set; }
+
+}
