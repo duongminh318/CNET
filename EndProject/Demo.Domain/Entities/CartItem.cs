9499d47066a001ba2892c7ccf20c11b87d36a995
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-10-27 16:54:52 +0700

    project. cart item (giỏ hàng)

---
+++ EndProject/Demo.Domain/Entities/CartItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Demo.Domain.Enums;
+
+namespace Demo.Domain.Entities
+{
+    [Table("CartItems")]
+    public class CartItem : DomainEntity<Guid>, IAuditTable
+    {
+        public Guid VariantId { get; set; }
+
+        [Column(TypeName = "nvarchar(1000)")]
+        public string VariantName { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal PurchasePrice { get; set; }
+        public Guid UserId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public EntityStatus Status { get; set; }
+    }
+}
