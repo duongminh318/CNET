93caa5df17c253c58334daaa2e80ab645c0c10c3
Duong Khoi Minh <dkminh_19th2@student.agu.edu.vn>
2024-11-03 15:36:21 +0700

    project. config sql (kêt nối csdl)

---
+++ EndProject/Demo.Persistence/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Demo.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Demo.Persistence;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddSqlServerPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var assembly = typeof(ApplicationDbContext).Assembly.GetName().Name;
+        Action<DbContextOptionsBuilder> builder = (option) =>
+        {
+            option.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"],
+                b => b.MigrationsAssembly(assembly));
+        };
+        services.AddDbContext<ApplicationDbContext>(builder);
+
+        services.AddIdentityCore<AppUser>()
+            .AddRoles<AppRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+
+        services.Configure<IdentityOptions>(options =>
+        {
+            options.Lockout.AllowedForNewUsers = true; // Default true
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2); // Default 5
+            options.Lockout.MaxFailedAccessAttempts = 3; // Default 5
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequiredUniqueChars = 1;
+            options.Lockout.AllowedForNewUsers = true;
+        });
+    }
+}
